572093b627a0b2440ea9d6545114f976dcf7defc
雨宮ゆきの <iamyukino@outlook.com>
2025-06-27 15:48:45 +0800

    sync valid_python_many

--- cnn_python/valid_python_many.py
+++ cnn_python/valid_python_many.py
@@ -1,18 +1,95 @@
-
 import os
-import argparse
+from tqdm import tqdm
 import numpy as np
 from PIL import Image
 
 from valid_python_single import read_bin_as_floats, func_conv2d, func_max_pooling, func_relu, func_fc, transform
 
+# 定义网络结构参数
+conv_shapes = [
+    (32, 3, 3, 3),   # conv1
+    (32, 32, 3, 3),   # conv2
+    (64, 32, 3, 3),   # conv3
+    (128, 64, 3, 3)   # conv4
+]
+fc_shapes = [
+    (256, 128 * 5 * 5),   # fc1
+    (128, 256),       # fc2
+    (7, 128)          # fc3
+]
+
+# 加载融合后的参数
+conv_weights = []
+conv_biases = []
+weight_conv_data = read_bin_as_floats('./params/weight_conv.bin')
+bias_conv_data = read_bin_as_floats('./params/bias_conv.bin')
+
+start_idx = 0
+for shape in conv_shapes:
+    size = np.prod(shape)
+    w = weight_conv_data[start_idx:start_idx+size].reshape(shape)
+    conv_weights.append(w)
+    start_idx += size
+
+start_idx = 0
+for shape in conv_shapes:
+    size = shape[0]
+    b = bias_conv_data[start_idx:start_idx+size]
+    conv_biases.append(b)
+    start_idx += size
+
+fc_weights = []
+fc_biases = []
+weight_fc_data = read_bin_as_floats('./params/weight_fc.bin')
+bias_fc_data = read_bin_as_floats('./params/bias_fc.bin')
+
+start_idx = 0
+for shape in fc_shapes:
+    size = np.prod(shape)
+    w = weight_fc_data[start_idx:start_idx+size].reshape(shape)
+    fc_weights.append(w)
+    start_idx += size
+
+start_idx = 0
+for shape in fc_shapes:
+    size = shape[0]
+    b = bias_fc_data[start_idx:start_idx+size]
+    fc_biases.append(b)
+    start_idx += size
+
+
+# 批量推理 (numpy 前向传播)
+def numpy_forward(x):
+    # 第一卷积块
+    x = func_conv2d(x, conv_weights[0], conv_biases[0], stride=1, padding=1)
+    x = func_relu(x)
+    x = func_conv2d(x, conv_weights[1], conv_biases[1], stride=1, padding=1)
+    x = func_relu(x)
+    x = func_max_pooling(x, kernel_size=2, stride=2)
+
+    # 第二卷积块
+    x = func_conv2d(x, conv_weights[2], conv_biases[2], stride=1, padding=1)
+    x = func_relu(x)
+    x = func_max_pooling(x, kernel_size=2, stride=2)
+
+    # 第三卷积块
+    x = func_conv2d(x, conv_weights[3], conv_biases[3], stride=1, padding=1)
+    x = func_relu(x)
+    x = func_max_pooling(x, kernel_size=2, stride=2)
+    output_conv = func_relu(x)
+
+    # 全连接块
+    x = x.reshape(x.shape[0], -1)  # 展平
+    x = func_fc(x, fc_weights[0], fc_biases[0])
+    x = func_relu(x)
+    x = func_fc(x, fc_weights[1], fc_biases[1])
+    x = func_relu(x)
+    output_fc = func_fc(x, fc_weights[2], fc_biases[2])
+
+    return output_fc
+
 
 if __name__ == '__main__':
-
-    weight_conv = np.array(read_bin_as_floats('./params/weight_conv.bin')).reshape(8, 3, 3, 3)
-    bias_conv   = np.array(read_bin_as_floats('./params/bias_conv.bin'))
-    weight_fc   = np.array(read_bin_as_floats('./params/weight_fc.bin')).reshape(7, 200)
-    bias_fc     = np.array(read_bin_as_floats('./params/bias_fc.bin'))
 
     # batch inference
     img_dir = './data/images'
@@ -21,17 +98,20 @@ if __name__ == '__main__':
     N = 100
     preds = []
     targets = []
-    with open(label_path, 'r', encoding = 'utf - 8') as f:
+    with open(label_path, 'r', encoding='utf - 8') as f:
         lines = f.readlines()
-        for line in lines[-N:]:
-            img_name, label = line.split(' ')
-            img = Image.open(os.path.join(img_dir, img_name))
-            img_tensor = transform(img).unsqueeze(0) 
-            output_conv = func_conv2d(img_tensor.squeeze().numpy().astype(np.float32), weight_conv, bias_conv, stride=1, padding=1)
-            output_conv = func_relu(func_max_pooling(output_conv, 8, 8))
-            output_fc   = func_fc(output_conv.reshape(1, -1), weight_fc, bias_fc)
-            preds.append(np.argmax(output_fc, 1).item())
-            targets.append(int(label)-1)
+        for line in tqdm(lines[-N:], desc="Processing images"):
+            img_name, label = line.strip().split(' ')
+            img = Image.open(os.path.join(img_dir, img_name)).convert('RGB')
+            img_tensor = transform(img).unsqueeze(0).numpy()  # [1, 3, 40, 40]
+
+            # 前向传播
+            output_fc = numpy_forward(img_tensor)
+
+            # 预测结果
+            predict = np.argmax(output_fc, axis=1)[0]
+            preds.append(predict)
+            targets.append(int(label)-1)  # 标签转换为0-based
 
     acc = 0
     for v1, v2 in zip(preds, targets):
